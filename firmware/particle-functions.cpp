7995971c71912fc7f90d4f7c807f76bc7a832d7b
geoffsharris <geoffsharris@yahoo.com>
2015-08-11 07:42:09 -0700

    Update particle-functions.cpp

--- firmware/particle-functions.cpp
+++ firmware/particle-functions.cpp
@@ -3,18 +3,25 @@
 
 void WeatherData :: message(int data)     // defined outside class definition
 {
- Serial.print(data);
- Serial.println(" F - current Temperature");
+ Serial.print("Timestamp: "); Serial.println(weatherTime);
+ Serial.print("Temp (F): "); Serial.println(greenhouseTemp);
+ Serial.print("Humidity (%): "); Serial.println(greenhouseHumidity);
 }
 
-void WeatherData :: weatherData()
-    {
-  
-    Serial.print("Timestamp: "); Serial.println(weatherTime);
-    Serial.print("Temp (F): "); Serial.println(greenhouseTemp);
-    Serial.print("Humidity (%): "); Serial.println(greenhouseHumidity);
-
-}
+void WeatherData :: weatherData(unsigned int weatherTime, int greenhouseTemp,int greenhouseHumidity,
+                                int backupGreenhouseTemp, int backupGreenhouseHumidity,int outsideTemp, 
+                                int outsideHumidity, int high, int low)
+ {
+ _weatherTime = weatherTime;
+ _greenhouseTemp = greenhouseTemp;
+ _greenhouseHumidity = greenhouseHumidity;
+ _backupGreenhouseTemp = backupGreenhouseTemp;
+ _backupGreenhouseHumidity = backupGreenhouseHumidity;
+ _outsideTemp = outsideTemp;
+ _outsideHumidity = outsideHumidity;
+ _high = high;
+ _low = low;
+ }
 
 void WeatherData :: init()
 {
