562c49afd98f937f5c5976e8497bea26639d13b7
geoffsharris <geoffsharris@yahoo.com>
2015-08-10 21:38:01 -0700

    Update particle-functions.cpp

--- firmware/particle-functions.cpp
+++ firmware/particle-functions.cpp
@@ -1,1 +1,21 @@
+# include "particle-functions.h"
+# include <application.h>
 
+
+void Weatherdata :: message(int data)     // defined outside class definition
+{
+ Serial.print(data);
+ Serial.println(" F - current Temperature");
+}
+
+void Weatherdata :: weatherData()
+    {
+  
+    Serial.print("Timestamp: "); Serial.println(weather_time);
+    Serial.print("Temp (F): "); Serial.println(GH_temp);
+    Serial.print("Humidity (%): "); Serial.println(GH_humidity);
+
+}
+
+
+ 
