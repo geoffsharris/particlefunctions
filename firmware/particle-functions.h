b15ba11b089902d71d2fd93e5ef31ca6d008c2d2
geoffsharris <geoffsharris@yahoo.com>
2015-08-10 21:45:02 -0700

    Update particle-functions.h

--- firmware/particle-functions.h
+++ firmware/particle-functions.h
@@ -1,1 +1,26 @@
+#ifndef WEATHERDATA_H
+#define WEATHERDATA_H
+#include <application.h>
 
+class WeatherData
+{
+ public:
+ unsigned int weatherTime;
+ int greenhouseTemp;
+ int greenhouseHumidity;
+ int outsideTemp;
+ int outsideHumidity;
+ int backupGreenhouseTemp;
+ int backupGreenhouseHumidity;
+ int high;
+ int low;
+ 
+ 
+ 
+ void message(int);
+ void weatherData();
+ void init();
+  
+};
+
+#endif
