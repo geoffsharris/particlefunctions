bd22b04495f736b88bef05c8e977fad34a381cb6
geoffsharris <geoffsharris@yahoo.com>
2015-08-23 09:26:03 -0700

    Update weather.cpp

--- firmware/weather.cpp
+++ firmware/weather.cpp
@@ -1,153 +1,61 @@
-# include "weather.h"
-# include <application.h>
+#ifndef WEATHERDATA_H
+#define WEATHERDATA_H
+#include <application.h>
 
+// setup class to hold weather data and functions to get data
+// load information by calling weatherData() and passing variables
+// get data by calling vaious functions to return data values
+// no need to initialize object in main portion of program
+// should set up object named "weather" to call for information - example weather.greenhouseTemp();
+// will return current greenhouse temeprature
+
+class WeatherData
+{
+ public:
+ // constructors
+ void weatherData();
+ void weatherData(unsigned int weatherTime, int greenhouseTemp,int greenhouseHumidity,
+                  int backupGreenhouseTemp, int backupGreenhouseHumidity,int outsideTemp,
+                  int outsideHumidity, int solar, int high, int low);
+ // methods 
+ void message(int);
+ unsigned int weatherTime();
+ int greenhouseTemp();
+ int greenhouseHumidity();
+ int backupGreenhouseTemp();
+ int backupGreenhouseHumidity();
+ int outsideTemp();
+ int outsideHumidity();
+ int solar();
+ int high();
+ int low();
+ 
+ // variables
+ unsigned int m_weatherTime;
+ int m_greenhouseTemp;
+ int m_greenhouseHumidity;
+ int m_backupGreenhouseTemp;
+ int m_backupGreenhouseHumidity;
+ int m_outsideTemp;
+ int m_outsideHumidity;
+ int m_solar;
+ int m_high;
+ int m_low;
+ 
+ 
+// private:
+ unsigned int _weatherTime;
+ int _greenhouseTemp;
+ int _greenhouseHumidity;
+ int _backupGreenhouseTemp;
+ int _backupGreenhouseHumidity;
+ int _outsideTemp;
+ int _outsideHumidity;
+ int _solar;
+ int _high;
+ int _low;
+  
+};
 extern WeatherData weather;
 
-void WeatherData :: message(int data)     // defined outside class definition
-{
- Serial.print("Timestamp: "); Serial.println(_weatherTime);
- Serial.print("Temp (F): "); Serial.println(_greenhouseTemp);
- Serial.print("Humidity (%): "); Serial.println(_greenhouseHumidity);
-}
-
-void WeatherData :: weatherData()
-{
-    _weatherTime = 0;
- _greenhouseTemp = 0;
- _greenhouseHumidity = 0;
- _backupGreenhouseTemp = 0;
- _backupGreenhouseHumidity = 0;
- _outsideTemp = 0;
- _outsideHumidity = 0;
-  _solar = 0;
- _high = 0;
- _low = 0;
-}
-
-void WeatherData :: weatherData(unsigned int weatherTime, int greenhouseTemp,int greenhouseHumidity,
-                                int backupGreenhouseTemp, int backupGreenhouseHumidity,int outsideTemp, 
-                                int outsideHumidity, int solar, int high, int low)
- {
- _weatherTime = weatherTime;
- _greenhouseTemp = greenhouseTemp;
- _greenhouseHumidity = greenhouseHumidity;
- _backupGreenhouseTemp = backupGreenhouseTemp;
- _backupGreenhouseHumidity = backupGreenhouseHumidity;
- _outsideTemp = outsideTemp;
- _outsideHumidity = outsideHumidity;
-  _solar = solar;
- _high = high;
- _low = low;
- }
-
-
-int greenhouseData(String data)
-    {
-  
-
-    //Expected parameters in CSV format
-    // 1. Unix time stamp
-    // 2. current greenhouse temperature
-    // 3. current greenhouse humidity
-    // 4. backup sensor current greenhouse temperature
-    // 5. backup sensor current greenhouse humidity
-    // 6. current outside temperature
-    // 7. current outside humidity
-    // 8. solar radiation
-    // 9. forecast high
-    // 10. forecast low
-    
-
-    char copyStr[64];
-    data.toCharArray(copyStr,64);
-    char *p = strtok(copyStr, ",");
-
-    unsigned int weatherTime = atoi(p);
-    p = strtok(NULL,",");
-    int greenhouseTemp = atoi(p);
-    p = strtok(NULL,",");
-    int greenhouseHumidity = atoi(p);
-    p = strtok(NULL,",");
-    int backupGreenhouseTemp = atoi(p);
-    p = strtok(NULL,",");
-    int backupGreenhouseHumidity = atoi(p);
-    p = strtok(NULL,",");
-    int outsideTemp = atoi(p);
-    p = strtok(NULL,",");
-    int outsideHumidity = atoi(p);
-    p = strtok(NULL,",");
-    int solar = atoi(p);
-    p = strtok(NULL,",");
-    int high = atoi(p);
-    p = strtok(NULL,",");
-    int low = atoi(p);
-
-    weather.weatherData(weatherTime, greenhouseTemp, greenhouseHumidity, backupGreenhouseTemp,
-                        backupGreenhouseHumidity, outsideTemp, outsideHumidity, solar, high, low);
-
-return 1;
-}
-
-int setSeason(String command) // pass CSV AMsetback, PMsetback, dayProgram, nightProgram
-{
-   char copyStr[64];
-    command.toCharArray(copyStr,64);
-    char *p = strtok(copyStr, ",");
-
-    AMsetback = atoi(p);
-    String AMsetback_string = p;
-    p = strtok(NULL,",");
-    String PMsetback_string = p;
-    PMsetback = atoi(p);
-    p = strtok(NULL,",");
-    dayProgram = atoi(p);
-    p = strtok(NULL,",");
-    nightProgram = atoi(p);
-
-    Alarm.free(0);
-    Alarm.free(1);
-    Alarm.alarmRepeat(AMsetback,0,0, MorningAlarm);
-    Alarm.alarmRepeat(PMsetback,0,0, EveningAlarm);
-    Spark.publish("AMsetback", AMsetback_string);
-    Spark.publish("PMsetback", PMsetback_string);
-
-return 1;
-}
-
-void particleInit()
-{
- // time function setup
-  Time.zone(tzOffset);
-  Alarm.alarmRepeat(AMsetback,0,0, MorningAlarm);
-  Alarm.alarmRepeat(PMsetback,0,0, EveningAlarm);
-  Alarm.alarmRepeat(0,0,0, synchTime);
-  
- // setup particle functions
- Spark.function("ghData", greenhouseData);
- Spark.function("test_call",call_function);
- Spark.function("setSeason", setSeason);
-}
-
-// variables used in program
-int tzOffset = -7;
-int AMsetback = 7;
-int PMsetback = 22;
-int dayProgram = 1;
-int nightProgram = 3;
-
-// functions called by time functions
-void MorningAlarm();
-{
- 
-}
-void EveningAlarm();
-{
- 
-}
-
-// called once per day at midnight to synch photon
-void synchTime();
-{
- Spark.syncTime();
-}
-
+#endif
